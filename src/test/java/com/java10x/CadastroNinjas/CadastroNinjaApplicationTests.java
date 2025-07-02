92a824583c16848a660ed8e123fe5632a6a2344e
Jose <xandin.blizz@gmail.com>
2025-07-02 16:49:36 -0300

    first commit

---
+++ src/test/java/com/java10x/CadastroNinjas/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java10x.CadastroNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
