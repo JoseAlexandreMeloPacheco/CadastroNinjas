5555dd7895bde82dd360539dff70a8cc738d54fe
Jose <xandin.blizz@gmail.com>
2025-07-03 10:22:00 -0300

    Cria ententidade no banco de dados aula3

--- src/main/java/com/java10x/CadastroNinjas/CadastroNinjaApplication.java
+++ src/main/java/com/java10x/CadastroNinjas/CadastroNinjaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  CadastroNinjaApplication {
+public class CadastroNinjaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroNinjaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroNinjaApplication.class, args);
+    }
 
 }
